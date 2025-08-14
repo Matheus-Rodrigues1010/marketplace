e6ce5b7a38c4b6bac953142ca7167106a00d5824
Matheus Rodrigues <rodriguesmatheus276@gmail.com>
2025-08-14 19:34:02 -0400

    fix: miss /api on url

--- frontend/src/apiConfig.ts
+++ frontend/src/apiConfig.ts
@@ -4,6 +4,6 @@
 // para verificar se estamos em ambiente de desenvolvimento.
 const apiUrl: string = import.meta.env.DEV
   ? 'http://localhost:3001/api' // URL para desenvolvimento local
-  : 'https://marketplace-api-cd30.onrender.com'; // <-- SUBSTITUA PELA SUA URL DA VERCEL
+  : 'https://marketplace-api-cd30.onrender.com/api'; // <-- SUBSTITUA PELA SUA URL DA VERCEL
 
 export default apiUrl;
